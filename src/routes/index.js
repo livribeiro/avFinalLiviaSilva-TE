722a68c555dc001fa034bd9dff098024ac334086
Livia <carolinabak2@gmail.com>
2021-07-08 17:14:22 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express =require('express')
 const customersRouter = require('./routers/routers')
 
-const app = express()
 
+const app = express()
 
 app.get('/', (req, res) => {
     res.send('Teste!!!!')
